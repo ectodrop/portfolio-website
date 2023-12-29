2df9c652e77a07ffb76489e4948cfde475e84a40
ectodrop <yanghoward1@gmail.com>
2023-12-29 15:46:01 -0600

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
